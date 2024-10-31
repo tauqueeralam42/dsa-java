d91d33824d6e3ffc6cbcfec6122b4f2f7fd050cb
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-10-31 20:39:23 +0530

    Add Character.java to print alphabet pattern

---
+++ 01.Pattern/Character.java
@@ -0,0 +1,20 @@
+public class Character {
+
+    // A
+    // BC
+    // DEF
+    // GHIJ
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        char ch = 'A';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
