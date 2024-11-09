5cdc93bef44e665def09acc4cab5fa697b2fe643
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-11-09 11:45:56 +0530

    Add Palindrome class with isPalindrome method

---
+++ 05.Strings/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome{
+
+    public static boolean isPalindrome(String str){
+        int i = 0, j = str.length() - 1;
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "madam";
+        System.out.println(isPalindrome(str));
+    }
+}
