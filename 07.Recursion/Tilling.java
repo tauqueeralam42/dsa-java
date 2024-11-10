ed043e3ab414f446a92efe951f8ae1c859165df8
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-11-10 22:44:44 +0530

    Add Tilling class with recursive method to calculate number of ways to tile a floor

---
+++ 07.Recursion/Tilling.java
@@ -0,0 +1,13 @@
+public class Tilling {
+    
+    public static int tilling(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return tilling(n - 1) + tilling(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tilling(4));
+    }
+}
