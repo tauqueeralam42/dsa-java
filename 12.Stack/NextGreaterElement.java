bdd212cae7bbec77068033b5161ec915e7c25253
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-11-16 18:49:10 +0530

    Add nextGreaterElementLeft method to find next greater elements to the left and rename existing method

--- 12.Stack/NextGreaterElement.java
+++ 12.Stack/NextGreaterElement.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class NextGreaterElement {
 
-    public static int[] nextGreaterElement(int[] arr){
+    public static int[] nextGreaterElementRight(int[] arr){
         Stack<Integer> s = new Stack<>();
         int n = arr.length;
         int[] ans = new int[n];
@@ -22,12 +22,42 @@ public class NextGreaterElement {
         }
         return ans;
     }
+
+    public static int[] nextGreaterElementLeft(int[] arr){
+        Stack<Integer> s = new Stack<>();
+        int n = arr.length;
+        int [] ans = new int[n];
+
+        for(int i = 0; i<n; i++){
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+
+    public static void printArray(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     
     public static void main(String[] args) {
         int[] arr = {6, 8, 0, 1, 3};
-        int[] res = nextGreaterElement(arr);
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i]+" ");
-        }
+        int[] right = nextGreaterElementRight(arr);
+        int[] left = nextGreaterElementLeft(arr);
+
+        System.out.println("Next Greater Element to the Right");
+        printArray(right);
+        System.out.println("Next Greater Element to the Left");
+        printArray(left);
+        
     }
 }
