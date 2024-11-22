16f1c747d18ff2ff2eafafa580a434b05d241498
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-11-22 20:41:25 +0530

    Refactor SubTree class for improved readability and formatting

--- 17.Trees/SubTree.java
+++ 17.Trees/SubTree.java
@@ -1,48 +1,46 @@
 public class SubTree {
 
-    public static class Node{
+    public static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
 
-    public static boolean isIdentical(Node root, Node subRoot){
-        if(root == null && subRoot == null){
+    public static boolean isIdentical(Node root, Node subRoot) {
+        if (root == null && subRoot == null) {
             return true;
-        }else if(root == null || subRoot == null || root.data != subRoot.data){
+        } else if (root == null || subRoot == null || root.data != subRoot.data) {
             return false;
         }
 
-        if(!isIdentical(root.left,subRoot.left))
+        if (!isIdentical(root.left, subRoot.left))
             return false;
-        if(!isIdentical(root.right,subRoot.right))
+        if (!isIdentical(root.right, subRoot.right))
             return false;
 
-            return true;
+        return true;
     }
 
-
-    public static boolean isSubTree(Node root, Node subRoot){
-        if(root == null){
+    public static boolean isSubTree(Node root, Node subRoot) {
+        if (root == null) {
             return false;
         }
 
-        if(root.data == subRoot.data){
-            if(isIdentical(root,subRoot))
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot))
                 return true;
         }
 
-        return isSubTree(root.left,subRoot) || isSubTree(root.right,subRoot);
+        return isSubTree(root.left, subRoot) || isSubTree(root.right, subRoot);
     }
-    
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Node root = new Node(1);
         root.left = new Node(2);
@@ -52,12 +50,11 @@ public class SubTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-
         Node subRoot = new Node(2);
         subRoot.left = new Node(4);
         subRoot.right = new Node(5);
 
-        System.out.println(isSubTree(root,subRoot));
-       
+        System.out.println(isSubTree(root, subRoot));
+
     }
 }
