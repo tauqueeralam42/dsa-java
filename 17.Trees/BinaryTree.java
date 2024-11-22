3771c293f170ffbf4ef4f76dfb8f6a78e078a536
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-11-22 16:50:45 +0530

    Add traversal methods (preOrder, postOrder, inOrder) to BinaryTree class

--- 17.Trees/BinaryTree.java
+++ 17.Trees/BinaryTree.java
@@ -27,10 +27,45 @@ public class BinaryTree {
         return newNode;
     }
 
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+        
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 6, -1, -1, 3, -1, 6, -1, -1 };
         Node root = buildTree(nodes);
-        System.out.println(root.data);
+        preOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
+        inOrder(root);
 
     }
 }
